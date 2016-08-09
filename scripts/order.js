134f8662cba5c00807f2fe479d2b2076a15c73eb
Allen Lin <allen830423@gmail.com>
2016-08-09 18:34:38 +0800

    compelte use id to find

--- scripts/order.js
+++ scripts/order.js
@@ -14,12 +14,30 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 
+var _extends = Object.assign || function (target) {
+  for (var i = 1; i < arguments.length; i++) {
+    var source = arguments[i];for (var key in source) {
+      if (Object.prototype.hasOwnProperty.call(source, key)) {
+        target[key] = source[key];
+      }
+    }
+  }return target;
+};
+
 var _drinks = require('../utils/drinks');
 
 var _drinks2 = _interopRequireDefault(_drinks);
 
 function _interopRequireDefault(obj) {
   return obj && obj.__esModule ? obj : { default: obj };
+}
+
+function _defineProperty(obj, key, value) {
+  if (key in obj) {
+    Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true });
+  } else {
+    obj[key] = value;
+  }return obj;
 }
 
 function _toConsumableArray(arr) {
@@ -36,9 +54,14 @@ exports.default = function (robot) {
   robot.brain.set('orders', []);
   robot.brain.set('isOrdering', false);
 
-  var startOrder = function startOrder() {
+  var startOrder = function startOrder(shopName) {
     robot.brain.set('isOrdering', true);
     robot.brain.set('orders', []);
+    robot.brain.set('menu', _drinks2.default[shopName]);
+  };
+
+  var getMenu = function getMenu() {
+    return robot.brain.get('menu');
   };
 
   var ordering = function ordering() {
@@ -66,32 +89,62 @@ exports.default = function (robot) {
     robot.brain.set('isOrdering', false);
   };
 
+  var listMatch = function listMatch() {
+    var orders = list();
+    var sum = orders.reduce(function (prev, o) {
+      var fullName = o.drink + ' ' + o.sugar + ' ' + o.ice;
+      var match = Object.keys(prev).find(function (k) {
+        return fullName === k;
+      });
+      if (match) {
+        return _extends({}, prev, _defineProperty({}, fullName, prev[fullName] + 1));
+      }
+      return _extends({}, prev, _defineProperty({}, fullName, 1));
+    }, {});
+    return Object.keys(sum).reduce(function (prev, o) {
+      return '' + prev + o + ' 有 ' + sum[o] + ' 杯\n';
+    }, '\n');
+  };
+
   var orders2String = function orders2String(orders) {
     return orders.reduce(function (prev, o) {
       return '' + prev + o.name + ' 點了 ' + o.drink + ' ' + o.sugar + ' ' + o.ice + '\n';
-    }, '');
+    }, '\n');
   };
 
-  robot.hear(/我要訂飲料/i, function (res) {
+  var drinks2String = function drinks2String(orders) {
+    return orders.reduce(function (prev, o) {
+      return '' + prev + o.id + ' ' + o.name + '\n';
+    }, '\n');
+  };
+
+  robot.hear(/我要訂飲料/, function (res) {
     var result = Object.keys(_drinks2.default).join('、');
     res.send('要訂哪間 ' + result);
   });
 
-  robot.hear(/^訂(.*)/i, function (res) {
+  robot.hear(/^訂(.*)/, function (res) {
     var shopName = res.match[1];
     if (_drinks2.default[shopName]) {
-      startOrder();
-      res.send(_drinks2.default[shopName]);
+      if (ordering()) {
+        res.send('不要再訂了');
+      } else {
+        startOrder(shopName);
+        res.send(drinks2String(_drinks2.default[shopName]));
+      }
     } else {
       res.send('沒有' + shopName + ' ◢▆▅▄▃-崩╰(〒皿〒)╯潰-▃▄▅▆◣\n\n        訂 ' + Object.keys(_drinks2.default).join('、'));
     }
   });
 
-  robot.hear(/(.*) (.[糖冰]) (.[糖冰])/i, function (res) {
+  robot.hear(/(.*) (.[糖冰]) (.[糖冰])/, function (res) {
     if (ordering()) {
       var sugar = res.match[2];
       var ice = res.match[3];
       var name = res.message.user.name;
+      console.log(getMenu().find(function (d) {
+        return d.id === +res.match[1];
+      }));
       var drink = res.match[1];
       order({ sugar: sugar, ice: ice, name: name, drink: drink });
       res.send(name + ' 訂的是 ' + drink + ' ' + sugar + ' ' + ice);
@@ -100,9 +153,25 @@ exports.default = function (robot) {
     }
   });
 
-  robot.hear(/截止/i, function (res) {
-    var orders = list();
+  robot.hear(/取消/, function (res) {
+    var cancelOrder = list().find(function (o) {
+      return o.name === res.message.user.name;
+    });
+    if (cancelOrder) {
+      res.send('已經取消 ' + res.message.user.name + ' 訂的' + cancelOrder.drink);
+      cancel(res.message.user.name);
+    } else {
+      res.send('你哪位？');
+    }
+  });
+
+  robot.hear(/截止/, function (res) {
+    res.send(listMatch());
     clear();
+  });
+
+  robot.hear(/統計/, function (res) {
+    var orders = list();
     res.send(orders2String(orders));
   });
 };
--- src/order.js
+++ src/order.js
@@ -15,10 +15,13 @@ export default (robot) => {
   robot.brain.set('orders', []);
   robot.brain.set('isOrdering', false);
 
-  const startOrder = () => {
+  const startOrder = (shopName) => {
     robot.brain.set('isOrdering', true);
     robot.brain.set('orders', []);
+    robot.brain.set('menu', shopsList[shopName]);
   };
+
+  const getMenu = () => robot.brain.get('menu');
 
   const ordering = () => robot.brain.get('isOrdering');
 
@@ -39,32 +42,55 @@ export default (robot) => {
     robot.brain.set('isOrdering', false);
   };
 
+  const listMatch = () => {
+    const orders = list();
+    const sum = orders.reduce((prev, o) => {
+      const fullName = `${o.drink} ${o.sugar} ${o.ice}`;
+      const match = Object.keys(prev).find(k => fullName === k);
+      if (match) {
+        return { ...prev, [fullName]: prev[fullName] + 1 };
+      }
+      return { ...prev, [fullName]: 1 };
+    }, {});
+    return Object.keys(sum).reduce((prev, o) => `${prev}${o} 有 ${sum[o]} 杯\n`
+      , '\n');
+  };
+
   const orders2String = (orders) =>
     orders.reduce((prev, o) => `${prev}${o.name} 點了 ${o.drink} ${o.sugar} ${o.ice}\n`
       , '\n');
 
-  robot.hear(/我要訂飲料/i, (res) => {
+  const drinks2String = (orders) =>
+    orders.reduce((prev, o) => `${prev}${o.id} ${o.name}\n`
+      , '\n');
+
+  robot.hear(/我要訂飲料/, (res) => {
     const result = Object.keys(shopsList).join('、');
     res.send(`要訂哪間 ${result}`);
   });
 
-  robot.hear(/^訂(.*)/i, (res) => {
+  robot.hear(/^訂(.*)/, (res) => {
     const shopName = res.match[1];
     if (shopsList[shopName]) {
-      startOrder();
-      res.send(shopsList[shopName]);
+      if (ordering()) {
+        res.send('不要再訂了');
+      } else {
+        startOrder(shopName);
+        res.send(drinks2String(shopsList[shopName]));
+      }
     } else {
       res.send(`沒有${shopName} ◢▆▅▄▃-崩╰(〒皿〒)╯潰-▃▄▅▆◣\n
         訂 ${Object.keys(shopsList).join('、')}`);
     }
   });
 
-  robot.hear(/(.*) (.[糖冰]) (.[糖冰])/i, (res) => {
+  robot.hear(/(.*) (.[糖冰]) (.[糖冰])/, (res) => {
     if (ordering()) {
       const sugar = res.match[2];
       const ice = res.match[3];
       const name = res.message.user.name;
-      const drink = res.match[1];
+      const id2drink = getMenu().find(d => d.id === +res.match[1]);
+      const drink = (id2drink) ? id2drink.name : res.match[1];
       order({ sugar, ice, name, drink });
       res.send(`${name} 訂的是 ${drink} ${sugar} ${ice}`);
     } else {
@@ -72,9 +98,23 @@ export default (robot) => {
     }
   });
 
-  robot.hear(/截止/i, (res) => {
-    const orders = list();
+  robot.hear(/取消/, (res) => {
+    const cancelOrder = list().find(o => o.name === res.message.user.name);
+    if (cancelOrder) {
+      res.send(`已經取消 ${res.message.user.name} 訂的${cancelOrder.drink}`);
+      cancel(res.message.user.name);
+    } else {
+      res.send('你哪位？');
+    }
+  });
+
+  robot.hear(/截止/, (res) => {
+    res.send(listMatch());
     clear();
+  });
+
+  robot.hear(/統計/, (res) => {
+    const orders = list();
     res.send(orders2String(orders));
   });
 };
