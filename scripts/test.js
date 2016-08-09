e9ec8bba565b0d5d935fd31b96baa492a4c01690
Allen Lin <allen830423@gmail.com>
2016-08-09 14:11:15 +0800

    test script

---
+++ scripts/test.js
@@ -0,0 +1,23 @@
+
+
+// Description:
+//   Show menus of tea shops
+//
+// Commands:
+//   drinks - 看有哪些飲料店
+//   drinks 飲料店 - 有個飲料店的菜單
+//
+// Author:
+//    Candy
+//
+export default (robot => {
+  robot.hear(/^drinks$/i, res => {
+    const result = 'drink water!!!';
+    res.send(result);
+  });
+
+  robot.hear(/drinks (.*)/i, res => {
+    const result = 'drink water!!!';
+    res.send(result);
+  });
+});
